a3937bed1a3a909b3174ec9a536ec09041c8368e
Cenk Yenikoylu <mcy@dotnetmcy.net>
2024-04-29 23:16:59 +0300

    first commit

---
+++ spotifav/src/script.ts
@@ -0,0 +1,124 @@
+const clientId = "30a68aef631144c8a81c6a26747292e4"; // Replace with your client id
+// const code = undefined;
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    console.log(accessToken);
+
+    const profile = await fetchProfile(accessToken);
+    console.log(profile); // Profile data logs to console
+    populateUI(profile);
+
+    const player = await fetchPlayer(accessToken);
+    console.log(player);
+    playerData(player);
+}
+
+export async function redirectToAuthCodeFlow(clientId: string) {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("scope", "user-read-private user-read-email user-read-currently-playing");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length: number) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier: string) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
+export async function getAccessToken(clientId: string, code: string): Promise<string> {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("code_verifier", verifier!);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
+}
+
+async function fetchProfile(token: string): Promise<any> {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+}
+
+function populateUI(profile: any) {
+    document.getElementById("displayName")!.innerText = profile.display_name;
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar")!.appendChild(profileImage);
+    }
+    document.getElementById("id")!.innerText = profile.id;
+    document.getElementById("email")!.innerText = profile.email;
+    document.getElementById("uri")!.innerText = profile.uri;
+    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url")!.innerText = profile.href;
+    document.getElementById("url")!.setAttribute("href", profile.href);
+    document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+}
+
+(document.getElementById('btnGetCurrentlyPlayingTrack')).addEventListener('click', () => {
+    console.log('Button clicked');
+});
+
+async function fetchPlayer(token: string): Promise<any> {
+    const result = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+}
+
+function playerData(player: any) {
+    document.getElementById("artistName")!.innerText = player.item.album.artists[0]?.name ?? '(no artist name)';
+    document.getElementById("trackName")!.innerText = player.item.name;
+    document.getElementById("albumname")!.innerText = player.item.album.name ?? '(no artist name)';
+    document.getElementById("albumImgUrl")!.innerText = player.item.album.images[0]?.url ?? '(no profile image)';
+    document.getElementById("trackUrl")!.setAttribute("href", player.item.external_urls.spotify);
+    if (player.item.album.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = player.item.album.images[0].url;
+        document.getElementById("albumAvatar")!.appendChild(profileImage);
+    }
+}
